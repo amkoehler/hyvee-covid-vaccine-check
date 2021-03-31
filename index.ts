7a4891d65f8b368972b1d25746bd120f0afbc290
Alex Koehler <alex@layer.team>
2021-03-31 13:31:50 -0500

    Sort locatinos by name

--- index.ts
+++ index.ts
@@ -92,7 +92,19 @@ async function run() {
           covidVaccineEligibilityTerms: location.covidVaccineEligibilityTerms,
         }));
 
-        console.table(locations);
+        console.table(
+          locations.sort((a, b) => {
+            if (a.name > b.name) {
+              return 1;
+            }
+
+            if (a.name < b.name) {
+              return -1;
+            }
+
+            return 0;
+          })
+        );
 
         const vaccinesAvailable = locations.some(
           (l) => l.isCovidVaccineAvailable
